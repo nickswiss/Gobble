def7601647598b68b4629b649ad05281201b58d1
nickswiss <nick@Nicks-iMac.local>
2015-11-23 16:52:46 -0500

    New Colors and Layout

--- Gobble/ViewController.swift
+++ Gobble/ViewController.swift
@@ -14,13 +14,14 @@ import FontAwesome_swift
 class ViewController: UIViewController {
     
     var sounds = ["Wing Bone", "Push Pull", "Scratch Box", "Slate Over Glass", "Crystal Over Slate"]
-    var turkeyPlayer : AVAudioPlayer = AVAudioPlayer()
+    var turkeyPlayer : AVAudioPlayer?
+    
     var callInfo = [
         "Wingbone calls originally were made from the wingbones of a turkey, and some still are. They are a suction-type call. Sounds are made with quick, forceful sucking motions, much like kissing the end of the call. Good wingbone calls make a hollow sounding yelp.",
         "Push-pull turkey calls are the simplest of all turkey calls to use, and create realistic turkey sounds. A push-pull call functions by pushing and/or pulling a button on the end of the call, forcing a surface across a peg.",
         "Box calls create turkey sounds with the friction created by sliding the lid across the surface of the box. Box calls are convenient and are capable of producing more volume than any other call in the world.",
         "The tube call is a popular caller for many of the nation's top turkey hunters. With it, a hunter can make virtually any sound in a turkey's vocabulary from yelps to purrs to gobbles. Tube calls consist of a small hollow barrel with latex fixed across half of the top with an elastic band.",
-        "Friction calls may be the most common turkey calls[citation needed] because they are easy to use and create lifelike turkey sounds. Friction calls feature a round (usually) surface, and the user creates sound by drawing a peg, or striker, across the surface. Friction call surfaces can be slate, aluminum, glass or a variety of other materials.",
+        "Friction calls feature a round (usually) surface, and the user creates sound by drawing a peg, or striker, across the surface. Friction call surfaces can be slate, aluminum, glass or a variety of other materials.",
     
     ]
     
@@ -31,7 +32,9 @@ class ViewController: UIViewController {
     @IBOutlet var infoLabel: UILabel!
     @IBOutlet var playButton: UIButton!
     @IBOutlet var stopButton: UIButton!
+    @IBOutlet var slideLabel: UILabel!
     
+    @IBOutlet var bottomView: UIView!
     
     /** Consraints for animation **/
     @IBOutlet var infoLeadingConstraint: NSLayoutConstraint!
@@ -39,6 +42,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        turkeyPlayer = AVAudioPlayer()
         infoLabel.text = callInfo[0]
         let swipeRight = UISwipeGestureRecognizer(target: self, action: "respondToSwipe:")
         swipeRight.direction = UISwipeGestureRecognizerDirection.Right
@@ -49,13 +53,20 @@ class ViewController: UIViewController {
         turkeyImageView.addGestureRecognizer(swipeLeft)
         turkeyImageView.userInteractionEnabled = true
         
-        playButton.titleLabel?.font = UIFont.fontAwesomeOfSize(72)
-        playButton.setTitle(String.fontAwesomeIconWithName(.PlayCircleO), forState: .Normal)
-
+        playButton.titleLabel?.font = UIFont.fontAwesomeOfSize(40)
+        playButton.setTitle(String.fontAwesomeIconWithName(.Play), forState: .Normal)
         
-        stopButton.titleLabel?.font = UIFont.fontAwesomeOfSize(48)
+        
+        stopButton.titleLabel?.font = UIFont.fontAwesomeOfSize(40)
         stopButton.setTitle(String.fontAwesomeIconWithName(.Stop), forState: .Normal)
-
+        
+        bottomView.layer.borderWidth = 1
+        bottomView.layer.borderColor = UIColor(red: 0.965, green: 0.902, blue: 0.80, alpha: 1.0).CGColor
+        
+        stopButton.enabled = false
+        stopButton.alpha = 0.5
+        
+    
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -106,16 +117,19 @@ class ViewController: UIViewController {
             } catch _ {
                 return print("file not found")
             }
-            
-            turkeyPlayer.numberOfLoops = 1
-            turkeyPlayer.prepareToPlay()
-            turkeyPlayer.play()
+            if let turkeyPlayer = turkeyPlayer {
+                turkeyPlayer.numberOfLoops = 1
+                turkeyPlayer.prepareToPlay()
+                turkeyPlayer.play()
+                stopButton.enabled = true
+                stopButton.alpha = 1.0
+            }
         }
     }
     
     @IBAction func stopPressed(sender: AnyObject) {
-        if turkeyPlayer.playing {
-            turkeyPlayer.stop()
+        if let tp = turkeyPlayer {
+            tp.stop()
         }
     }
 
@@ -125,24 +139,21 @@ class ViewController: UIViewController {
             switch swipeGesture.direction {
             case UISwipeGestureRecognizerDirection.Right:
                 if !infoRight {
-                    self.view.layoutIfNeeded()
+                    self.slideLabel.text = "<<<"
+                    self.infoLeadingConstraint.constant -= self.view.bounds.width
+                    self.infoTrailingConstraint.constant -= self.view.bounds.width
                     UIView.animateWithDuration(0.5, animations: {
-                        self.infoLeadingConstraint.constant -= self.view.bounds.width
-                        self.infoTrailingConstraint.constant -= self.view.bounds.width
                         self.view.layoutIfNeeded()
-                        print("Leading: \(self.infoLeadingConstraint.constant)")
-                        print("Trailing: \(self.infoTrailingConstraint.constant)")
                     })
                     infoRight = true
                 }
             case UISwipeGestureRecognizerDirection.Left:
                 if infoRight {
+                    self.slideLabel.text = ">>>"
+                    self.infoLeadingConstraint.constant += self.view.bounds.width
+                    self.infoTrailingConstraint.constant += self.view.bounds.width
                     UIView.animateWithDuration(0.5, animations: {
-                        self.infoLeadingConstraint.constant += self.view.bounds.width
-                        self.infoTrailingConstraint.constant += self.view.bounds.width
                         self.view.layoutIfNeeded()
-                        print("Leading: \(self.infoLeadingConstraint.constant)")
-                        print("Trailing: \(self.infoTrailingConstraint.constant)")
                     })
                     infoRight = false
                 }
@@ -155,6 +166,7 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
         return 1;
     }
@@ -167,10 +179,17 @@ extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
         return sounds[row]
     }
     
+    func pickerView(pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let sliderColor = UIColor(red: 0.965, green: 0.902, blue: 0.80, alpha: 1.0)
+        return NSAttributedString(string: sounds[row], attributes: [NSForegroundColorAttributeName:sliderColor])
+    }
+    
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        //TODO: Stop music when switch occurs
 
+        //TODO: Stop music when switch occurs
+        
         var image: UIImage = UIImage()
+        
         switch(row) {
         case 0:
             image = UIImage(named: "wingbone.jpg")!
@@ -193,7 +212,6 @@ extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
 
         infoLabel.text = callInfo[row]
         turkeyImageView.image = image
-  
     }
 }
 
